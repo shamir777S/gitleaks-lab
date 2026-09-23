d0f342ea9a27d72aa2d665cde93099c2837b602d
Yannick Sanon <yannicksanon@MacBook-Air-de-Yannick.local>
2026-09-23 17:22:15 +0200

    Remove secrets from source code

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-API_TOKEN = "REDACTED_API_TOKEN"
-
 def connect():
-print("Application started")
+    print("Application started")
 
 if __name__ == "__main__":
-connect()
-ANOTHER_TOKEN = "REDACTED_API_TOKENWXYZ"
+    connect()
